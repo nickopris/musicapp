e4d4caf39464dcd9d62c9d202196a8fd1ebf9df0
Jon Atxa <jon.atxa@global3digital.com>
2013-10-10 08:51:32 +0100

    playing with loops

--- js/main.js
+++ js/main.js
@@ -19,3 +19,24 @@ function SecondCtrl($scope, Data){
     return message.split("").reverse().join("");
   };
 }
+function DiscographyCtrl($scope){
+  $scope.songs = [
+    {"title":"song title 1", "album": "album name 1"},
+    {"title":"song title 2", "album": "album name 2"},
+    {"title":"song title 3", "album": "album name 3"}
+  ];
+}
+
+function JsonCtrl($scope, $http){
+  $scope.url = "http://www.3i.com/our-people/json";
+
+  $scope.addPeople = function(){
+    console.log("gets here: ");
+    $http.get($scope.url).then(function(response){
+            console.log("response: ",response);
+            queries = response.queries;
+            $scope.jsonData = queries.nodes.node;
+            $scope.message = queries.nodes.node.business;
+        });
+  };
+}
