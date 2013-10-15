b25eea8e1b6e0dc03798d40244fbb2a1231a7388
Jon Atxa <atxajon@gmail.com>
2013-10-15 21:29:04 +0100

    jsonp data retrieve through button

--- js/main.js
+++ js/main.js
@@ -29,7 +29,11 @@ function DiscographyCtrl($scope){
 
 function JSONPctrl($scope, $http) {
   var url = 'https://gdata.youtube.com/feeds/api/users/orbitalofficial/uploads?alt=json-in-script&callback=JSON_CALLBACK';
-  $http.jsonp(url).success(function(data) {
-      $scope.results = data.feed.entry;
-  });
+
+  $scope.getSongs = function(){
+    $http.jsonp(url).success(function(data) {
+        $scope.results = data.feed.entry;
+    });
+  };
+
 }
